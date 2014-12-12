2bc15e7371fedb9ff21e1e24536ad347cfb34b39
vagrant <vagrant@precise32.(none)>
2014-12-12 18:48:52 +0000

    Added ROS Hydro workspace for robot

---
+++ rockie_workspace/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/vagrant/wpi_sample_return_challenge_2015/rockie_workspace/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/hydro".split(';') if "/opt/ros/hydro" != "" else []
---
+++ rockie_workspace/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/vagrant/wpi_sample_return_challenge_2015/rockie_workspace/build/gtest
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
