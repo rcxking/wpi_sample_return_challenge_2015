1bcdd607c8e98e7eae277b7434fbf6dc68162dd2
Samson <bonfas@rpi.edu>
2015-06-05 22:30:58 -0400

    IT WORKS AT SERIAL STUFF

--- RockRaidersPSOCCode/RockRaidersPSOCCode.cydsn/main.c
+++ RockRaidersPSOCCode/RockRaidersPSOCCode.cydsn/main.c
@@ -1,5 +1,6 @@
 #include <project.h>
 #include <stdlib.h>
+#include <stdio.h>
 uint8 line[128];
 uint8 linesz=0;
 int motors[12];
@@ -24,17 +25,15 @@ int main()
     for(;;)
     {
         CyDelay(100);
-        /*if(linesz !=0)
+        if(linesz !=0)
         {
-            if(line[linesz]=='\n')
+            if(line[linesz-1]=='\n' || line[linesz-1]=='\r')
             {
                 parseSerial(line,linesz);
                 line[linesz]='\0';
-                linesz=0;
-                
-            }   
+                linesz=0;                
+            } 
         }
-        PCComms_UartPutChar(line[linesz]);*/
     }
 }
 
@@ -46,25 +45,23 @@ void parseSerial(uint8 *data,uint8 datasz)
     char *val= strtok_r(NULL,"\0",&itr); //Speed is anything after the first dash.
     int value = atoi(val);
     int motor= atoi(name);
+    char buff[255];
     if(value==255)
     {
-      PCComms_UartPutString("Motor ");
-      PCComms_UartPutChar(motor);
-      PCComms_UartPutString(" is set to speed ");
-      PCComms_UartPutChar(motors[motor]);
-      PCComms_UartPutCRLF(1);
+      snprintf(buff,255,"Motor %d is set to speed %d\r\n",motor,motors[motor]);
+      PCComms_UartPutString(buff);
     }
     else if(value<128&&value>-128) //If it's an acceptable value
     {      
       motors[motor]=value;
-      //char buff[128];
-      //sprintf(buff,"Setting motor %d to speed %d\r\n",motor,value);
       //SetMotor(motor,value);
-      PCComms_UartPutString("Setting motor ");
-      PCComms_UartPutChar(motor);
-      PCComms_UartPutString(" to speed ");
-      PCComms_UartPutChar(value);
-      PCComms_UartPutCRLF(1);
+      snprintf(buff,255,"Setting motor %d to speed %d\r\n",motor,value);
+      PCComms_UartPutString(buff);
+    }
+    else
+    {
+       snprintf(buff,255,"Bad data. Got: motor:%s value:%s\r\n",motor,value);
+       PCComms_UartPutString(buff);
     }
 }
 
