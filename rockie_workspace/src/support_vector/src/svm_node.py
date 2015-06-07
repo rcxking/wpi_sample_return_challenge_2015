1a1bbd4145418c3182cc597cdab7bba3275d4fbe
Bryant Pong <rcxking@gmail.com>
2015-06-07 17:39:51 -0400

    Rectifying Observation message

---
+++ rockie_workspace/src/support_vector/src/svm_node.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+'''
+svm_node.py - This node uses the support vector machine for Phase 1.         
+
+RPI Rock Raiders
+6/7/15
+
+Last Updated: Bryant Pong: 6/7/15 - 5:28 PM
+'''
+
+# Python Imports
+import rospy
+import sklearn.svm as svm
+from object_tracker.msg import * 
+
+# Main Node:
+def svmNode():
+	return
+
+if __name__ == "__main__":
+	svmNode()		 
