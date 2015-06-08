9e7ee2a45e8bf5d29b58fece757df053291142b4
rockie <rockie>
2015-06-07 22:40:09 -0400

    updates to tracker

--- rockie_workspace/src/object_tracker/src/tracker.py
+++ rockie_workspace/src/object_tracker/src/tracker.py
@@ -14,15 +14,27 @@ import rospy
 import cluster_util as cu
 from object_tracker.msg import Observation
 
+# extract intrinsic matrix from p matrix
+def pToIntrinsic(p):
+  P = numpy.array(p).reshape(3,4)
+  return P[0:3,0:3]
+
 class ObjectTracker:
   def __init__(self):
     self.loadParams()
 
-    rospy.Subscriber('observation', Observation)
+    rospy.Subscriber('observation', Observation, self.observationCallback)
     #self.service = rospy.ServiceProxy('service', Steer)
 
   def loadParams(self):
     self.param = rospy.get_param('param', 0.2)
+
+  def observationCallback(self, msg):
+    I = pToIntrinsic(msg.P)
+    t = msg.header.stamp
+    frame_id = msg.header.frame_id
+    point = np.array(msg.point)
+    pass
 
 if __name__ == "__main__":
   rospy.init_node('tracker')
