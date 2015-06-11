2450d7c1ba9d39a2cd9e8025ddc1b33308905325
rockie <rockie>
2015-06-11 11:02:03 -0400

    not using ar tags any more

---
+++ rockie_workspace/src/rockie_core/src/bag_cameras.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+record -o ~/data /stereo_camera/left/image_raw/compressed /stereo_camera/right/image_raw/compressed                      /stereo_camera/left/camera_info /stereo_camera/right/camera_info /tf /left_camera/image_raw/compressed /right_camera/image_raw/compressed /left_camera/camera_info /right_camera/camera_info
